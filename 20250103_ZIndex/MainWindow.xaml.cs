67db29b70a66fb764d7b84cfc0ea3791e16b42b5
waten <gogowaten@yahoo.co.jp>
2025-01-03 17:38:37 +0900

    202501031738

---
+++ 20250103_ZIndex/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20250103_ZIndex
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        private void MyButtonTest_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void MyButtonZTop_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyRootGroup.MyFocusThumb is KisoThumb kiso)
+            {
+                kiso.ZIndexTop();
+            }
+        }
+
+        private void MyButtonZUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyRootGroup.MyFocusThumb is KisoThumb kiso)
+            {
+                kiso.ZIndexUp();
+            }
+        }
+
+        private void MyButtonZDown_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyRootGroup.MyFocusThumb is KisoThumb kiso)
+            {
+                kiso.ZIndexDown();
+            }
+        }
+
+        private void MyButtonZBottom_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyRootGroup.MyFocusThumb is KisoThumb kiso)
+            {
+                kiso.ZIndexBottom();
+            }
+        }
+    }
+}
