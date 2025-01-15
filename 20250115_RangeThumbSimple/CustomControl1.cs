8c1489fe44cfc0e8e67942f3d2f83cd7ba952ba4
waten <gogowaten@yahoo.co.jp>
2025-01-15 20:57:18 +0900

    202501152057

--- 20250115_RangeThumbSimple/CustomControl1.cs
+++ 20250115_RangeThumbSimple/CustomControl1.cs
@@ -19,6 +19,8 @@ namespace _20250115_RangeThumbSimple
     public class RangeThumb : Thumb
     {
         private Thumb MyThumb = new() { Width = 20, Height = 20 };
+        private const double MinimumSize = 1;
+        private const double ZeroLocate = 0;
         static RangeThumb()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(RangeThumb), new FrameworkPropertyMetadata(typeof(RangeThumb)));
@@ -26,31 +28,59 @@ namespace _20250115_RangeThumbSimple
         public RangeThumb()
         {
             MyThumb.DragDelta += MyThumb_DragDelta;
+            DragDelta += RangeThumb_DragDelta;
+            Canvas.SetLeft(MyThumb, 0);
+            Canvas.SetTop(MyThumb, 0);
+            Canvas.SetLeft(this, 0);
+            Canvas.SetTop(this, 0);
+        }
+
+
+        private void RangeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is Thumb t)
+            {
+                //マイナス座標にならないように自身の移動
+                Canvas.SetLeft(t, Math.Max(ZeroLocate, Canvas.GetLeft(t) + e.HorizontalChange));
+                Canvas.SetTop(t, Math.Max(ZeroLocate, Canvas.GetTop(t) + e.VerticalChange));
+                e.Handled = true;
+            }
         }
 
         private void MyThumb_DragDelta(object sender, DragDeltaEventArgs e)
         {
             if (sender is Thumb t)
             {
-                var left = Canvas.GetLeft(t) + e.HorizontalChange;
-                var top = Canvas.GetTop(t) + e.VerticalChange;
-                if (left < 0) { left = 0; }
-                if (top < 0) { top = 0; }
-                Canvas.SetLeft(t, left);
-                Canvas.SetTop(t, top);
+                //1未満にならないようにThumbの移動
+                Canvas.SetLeft(t, Math.Max(MinimumSize, Canvas.GetLeft(t) + e.HorizontalChange));
+                Canvas.SetTop(t, Math.Max(MinimumSize, Canvas.GetTop(t) + e.VerticalChange));
+                e.Handled = true;
             }
         }
 
         public override void OnApplyTemplate()
         {
+            //Templateの中のCanvasを取得してMyThumbを追加とBinding処理
             base.OnApplyTemplate();
-            if (GetTemplateChild("PART_MyCanvas") is Canvas panel)
+            if (GetTemplateChild("PART_Canvas") is Canvas panel)
             {
                 panel.Children.Add(MyThumb);
-                MyThumb.SetBinding(Canvas.LeftProperty, new Binding() { Source = panel, Path = new PropertyPath(WidthProperty), Mode = BindingMode.TwoWay });
-                MyThumb.SetBinding(Canvas.TopProperty, new Binding() { Source = panel, Path = new PropertyPath(HeightProperty), Mode = BindingMode.TwoWay });
+                //Thumbの位置が自身の右下になるようにBinding
+                _ = MyThumb.SetBinding(Canvas.LeftProperty, new Binding()
+                {
+                    Source = this,
+                    Path = new PropertyPath(WidthProperty),
+                    Mode = BindingMode.TwoWay
+                });
 
+                _ = MyThumb.SetBinding(Canvas.TopProperty, new Binding()
+                {
+                    Source = this,
+                    Path = new PropertyPath(HeightProperty),
+                    Mode = BindingMode.TwoWay
+                });
             }
         }
+
     }
 }
