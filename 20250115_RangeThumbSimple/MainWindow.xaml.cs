dd50a366acd9e8fb8b88ee5780daf3464c668c36
waten <gogowaten@yahoo.co.jp>
2025-01-17 13:35:28 +0900

    202501171335blog

--- 20250115_RangeThumbSimple/MainWindow.xaml.cs
+++ 20250115_RangeThumbSimple/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System.Windows;
 
+//WPF、サイズ可変＋マウスドラッグ移動可能なCanvasを「できるだけ簡易」にカスタムコントロールで作ってみた - 午後わてんのブログ
+//https://gogowaten.hatenablog.com/entry/2025/01/17/133424
+
+
 namespace _20250115_RangeThumbSimple
 {
     public partial class MainWindow : Window
