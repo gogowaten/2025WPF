c2b83e07b8b870ca2e26aa3233a22217c3ce7d25
waten <gogowaten@yahoo.co.jp>
2025-01-24 15:08:59 +0900

    202501241508blog

--- 20250124_PathGeomerty/MainWindow.xaml.cs
+++ 20250124_PathGeomerty/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Media;
+//WPF、カスタムコントロールのThumbに表示するPolylineはGridよりCanvasに乗せると都合がいい - 午後わてんのブログ
+//https://gogowaten.hatenablog.com/entry/2025/01/23/125044
 
 namespace _20250124_PathGeomerty
 {
