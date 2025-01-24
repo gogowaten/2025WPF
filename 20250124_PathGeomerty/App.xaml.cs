7493caa6e1bbaed6afef3188b4cb6983485c02b2
waten <gogowaten@yahoo.co.jp>
2025-01-24 14:54:31 +0900

    202501241454

---
+++ 20250124_PathGeomerty/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _20250124_PathGeomerty
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 20250124_PathGeomerty/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace _20250124_PathGeomerty
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComboFillRule.ItemsSource = Enum.GetValues(typeof(FillRule));
+        }
+    }
+}
