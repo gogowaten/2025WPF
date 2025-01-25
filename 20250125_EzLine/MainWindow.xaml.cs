dfee1328c5fc4407215a149155f9e347fa64ea80
waten <gogowaten@yahoo.co.jp>
2025-01-25 18:07:20 +0900

    202501251807blog

--- 20250125_EzLine/MainWindow.xaml.cs
+++ 20250125_EzLine/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+//WPF、折れ線図形を描画するクラスをFrameworkElementを継承して作ってみた、線の太さを考慮してサイズ計算 - 午後わてんのブログ
+//https://gogowaten.hatenablog.com/entry/2025/01/25/180538
 
 namespace _20250125_EzLine
 {
@@ -12,7 +14,7 @@ namespace _20250125_EzLine
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Random r = new();
-            MyEzLine.MyPoints.Add(new Point(r.Next(300),r.Next(300)));
+            MyEzLine.MyPoints.Add(new Point(r.Next(300), r.Next(300)));
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
