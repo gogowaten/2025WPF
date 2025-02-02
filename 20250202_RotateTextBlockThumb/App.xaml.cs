c22a67c1645ce7573e3e573bba7cc7c25b1d21db
waten <gogowaten@yahoo.co.jp>
2025-02-02 15:01:12 +0900

    202502021501

---
+++ 20250202_RotateTextBlockThumb/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _20250202_RotateTextBlockThumb
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
