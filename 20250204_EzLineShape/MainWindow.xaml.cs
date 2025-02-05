d353307606a269d9b7754b303074188c960dc003
waten <gogowaten@yahoo.co.jp>
2025-02-05 16:19:46 +0900

    202502051619blog

--- 20250204_EzLineShape/MainWindow.xaml.cs
+++ 20250204_EzLineShape/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Media;
+//WPF、簡単に折れ線描画できて見た目通りのサイズと位置が取得できるクラスをShapeクラス継承で - 午後わてんのブログ
+//https://gogowaten.hatenablog.com/entry/2025/02/05/161559
 
 namespace _20250204_EzLineShape
 {
