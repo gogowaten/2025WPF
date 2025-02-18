659e6bf399cee44b98fda508ff13b7aa815fe27f
waten <gogowaten@yahoo.co.jp>
2025-02-18 17:32:28 +0900

    202502181732

--- 20250218_DashedBorderTextThumb/CustomControl1.cs
+++ 20250218_DashedBorderTextThumb/CustomControl1.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static _20250218_DashedBorderTextThumb.DashBorder;
 
 namespace _20250218_DashedBorderTextThumb
 {
