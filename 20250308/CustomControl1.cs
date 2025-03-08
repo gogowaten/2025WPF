f336af47818f93b3ae5cd20ce709f64d31dc9631
waten <gogowaten@yahoo.co.jp>
2025-03-08 16:23:19 +0900

    202503081623

---
+++ 20250308/CustomControl1.cs
@@ -0,0 +1,443 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20250308
+{
+
+
+
+    public abstract class EzShapeThumb : Thumb
+    {
+        static EzShapeThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EzShapeThumb), new FrameworkPropertyMetadata(typeof(EzShapeThumb)));
+        }
+        public EzShapeThumb()
+        {
+            DragDelta += EzShapeThumb_DragDelta;
+            Loaded += EzShapeThumb_Loaded;
+            Canvas.SetLeft(this, 0);
+        }
+
+        #region 起動時
+
+        private void EzShapeThumb_Loaded(object sender, RoutedEventArgs e)
+        {
+            //SetMyBind();
+            //Relayout();
+            UpdatePointsAndSizeWithoutZeroFix();
+        }
+
+        private void EzShapeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Canvas.SetLeft(this, Canvas.GetLeft(this) + e.HorizontalChange);
+            Canvas.SetTop(this, Canvas.GetTop(this) + e.VerticalChange);
+            e.Handled = true;
+        }
+
+        //起動時、Templateの中からCanvasとEzShapeを取得
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (GetTemplateChild("PART_Canvas") is Canvas panel)
+            {
+                MyPART_Canvas = panel;
+                if (GetChildEzShape(panel) is EzShape shape)
+                {
+                    MyEzShape = shape;
+                    if (MyPoints is null)
+                    {
+                        MyPoints = MyEzShape.MyPoints;
+                    }
+                    else
+                    {
+                        MyEzShape.MyPoints = MyPoints;
+                    }
+                }
+            }
+        }
+
+        private static EzShape? GetChildEzShape(FrameworkElement element)
+        {
+            var count = VisualTreeHelper.GetChildrenCount(element);
+            for (int i = 0; i < count; i++)
+            {
+                if (VisualTreeHelper.GetChild(element, i) is EzShape shape)
+                {
+                    return shape;
+                }
+            }
+            return null;
+        }
+
+        #endregion 起動時
+
+        #region 依存関係プロパティ
+
+
+
+
+        //内部図形のアンカーポイント表示用のAdorner
+        public EzShapeAdorner? MyEzShapeAdorner
+        {
+            get { return (EzShapeAdorner)GetValue(MyEzShapeAdornerProperty); }
+            protected set { SetValue(MyEzShapeAdornerProperty, value); }
+        }
+        public static readonly DependencyProperty MyEzShapeAdornerProperty =
+            DependencyProperty.Register(nameof(MyEzShapeAdorner), typeof(EzShapeAdorner), typeof(EzShapeThumb), new PropertyMetadata(null));
+
+
+        //確認用なので必要ない
+        public Canvas MyPART_Canvas
+        {
+            get { return (Canvas)GetValue(MyPART_CanvasProperty); }
+            protected set { SetValue(MyPART_CanvasProperty, value); }
+        }
+        public static readonly DependencyProperty MyPART_CanvasProperty =
+            DependencyProperty.Register(nameof(MyPART_Canvas), typeof(Canvas), typeof(EzShapeThumb), new PropertyMetadata(null));
+
+
+        public PointCollection MyPoints
+        {
+            get { return (PointCollection)GetValue(MyPointsProperty); }
+            set { SetValue(MyPointsProperty, value); }
+        }
+        public static readonly DependencyProperty MyPointsProperty =
+            DependencyProperty.Register(nameof(MyPoints), typeof(PointCollection), typeof(EzShapeThumb), new PropertyMetadata(null));
+
+
+        public EzShape MyEzShape
+        {
+            get { return (EzShape)GetValue(MyEzShapeProperty); }
+            set { SetValue(MyEzShapeProperty, value); }
+        }
+        public static readonly DependencyProperty MyEzShapeProperty =
+            DependencyProperty.Register(nameof(MyEzShape), typeof(EzShape), typeof(EzShapeThumb), new PropertyMetadata(null));
+        #endregion 依存関係プロパティ
+
+
+        /// <summary>
+        /// 再描画、不完全
+        /// </summary>
+        public void Relayout()
+        {
+            if (MyEzShape != null)
+            {
+                var myrect = GetBoundsFromPointsAndAnchorThumb();
+                var r4 = MyEzShape.MyBounds4;
+                //自身のサイズを変更
+                this.Width = r4.Width;
+                this.Height = r4.Height;
+
+                //変更する前の位置を使って計算しておく、タイミング重要
+                double tasLeft = Canvas.GetLeft(MyEzShape) + Canvas.GetLeft(this) + r4.Left;
+                double tasTop = Canvas.GetTop(MyEzShape) + Canvas.GetTop(this) + r4.Top;
+
+                //図形の位置を変更、オフセット
+                Canvas.SetLeft(MyEzShape, -r4.Left);
+                Canvas.SetTop(MyEzShape, -r4.Top);
+
+                //自身の位置を変更、図形の位置に合わせる
+                Canvas.SetLeft(this, tasLeft);
+                Canvas.SetTop(this, tasTop);
+            }
+        }
+
+        //
+        /// <summary>
+        ///頂点移動後に実行
+        ///Thumbのサイズと位置を更新する、アンカーポイント表示の有無で変化する
+        ///処理の順番は
+        ///MyPointsのBoundsが0,0になるように全体を移動、
+        ///アンカーポイントも移動、
+        ///Layout更新、
+        ///Thumbサイズ更新、
+        ///内部図形の移動、
+        ///Thumbの移動 
+        /// </summary>
+        public void UpdatePointAndSize()
+        {
+            var (left, top) = GetTopLeftFromPoints();
+            FixPointsZero(left, top);// PointsのゼロFix移動
+            FixAdornerLocate();// AdornerをPointsの表示位置に合わせる
+            UpdateLayout();// 要る？→必要            
+
+            var pointsRect = GetBoundsFromAnchorThumb();
+            //var r4 = MyEzShape.MyBounds4;//確認用
+            var unionR = MyEzShape.MyBounds4;
+            if (MyEzShapeAdorner != null)
+            {
+                unionR.Union(pointsRect);
+            }
+            Width = unionR.Width;
+            Height = unionR.Height;
+            //内部図形の位置の変更する前に今の位置を取得しておく
+            var ll = Canvas.GetLeft(MyEzShape) + unionR.Left + left;
+            var tt = Canvas.GetTop(MyEzShape) + unionR.Top + top;
+            ll += Canvas.GetLeft(this);
+            tt += Canvas.GetTop(this);
+
+            SetLocate(MyEzShape, -unionR.Left, -unionR.Top);
+            SetLocate(this, ll, tt);
+        }
+
+        //MyPointsのゼロFixなしでのサイズと位置更新、不完全
+        public void UpdatePointsAndSizeWithoutZeroFix()
+        {
+            var pointsRect = GetBoundsFromAnchorThumb();
+            var r4 = MyEzShape.MyBounds4;
+            var unionR = new Rect();
+            unionR.Union(pointsRect);
+            unionR.Union(r4);
+            Width = unionR.Width;
+            Height = unionR.Height;
+            //内部図形の位置の変更する前に今の位置を取得しておく
+            var ll = Canvas.GetLeft(MyEzShape) + unionR.Left;
+            var tt = Canvas.GetTop(MyEzShape) + unionR.Top;
+            ll += Canvas.GetLeft(this);
+            tt += Canvas.GetTop(this);
+
+            SetLocate(MyEzShape, -unionR.Left, -unionR.Top);
+            SetLocate(this, ll, tt);
+        }
+
+        public void UpdatePointsAndSizeWithoutZeroFixTest()
+        {
+            var (left, top) = GetTopLeftFromPoints();
+            FixPointsZero(left, top);// PointsのゼロFix移動
+            FixAdornerLocate();// AdornerをPointsの表示位置に合わせる
+            UpdateLayout();// 要る？→必要            
+
+            var pointsRect = GetBoundsFromAnchorThumb();
+            var r4 = MyEzShape.MyBounds4;
+            var unionR = MyEzShape.MyBounds4;
+            if (MyEzShapeAdorner != null)
+            {
+                unionR.Union(pointsRect);
+            }
+            Width = unionR.Width;
+            Height = unionR.Height;
+            //内部図形の位置の変更する前に今の位置を取得しておく
+            var ImaShapeLeft = Canvas.GetLeft(MyEzShape);
+            var ImaShapeTop = Canvas.GetTop(MyEzShape);
+            var ll = Canvas.GetLeft(MyEzShape) + unionR.Left + left;
+            var tt = Canvas.GetTop(MyEzShape) + unionR.Top + top;
+            ll += Canvas.GetLeft(this);
+            tt += Canvas.GetTop(this);
+
+            SetLocate(MyEzShape, -unionR.Left, -unionR.Top);
+            SetLocate(this, ll, tt);
+        }
+
+
+        private void SetLocate(FrameworkElement element, double left, double top)
+        {
+            Canvas.SetLeft(element, left);
+            Canvas.SetTop(element, top);
+        }
+
+        /// <summary>
+        /// アンカーハンドルの表示切替
+        /// </summary>
+        public void AnchorOnOffSwitch()
+        {
+            if (AdornerLayer.GetAdornerLayer(MyEzShape) is AdornerLayer layer)
+            {
+                //無ければ追加(表示)
+                if (MyEzShapeAdorner is null)
+                {
+                    EzShapeAdorner adorner = new(MyEzShape);
+                    layer.Add(adorner);
+                    MyEzShapeAdorner = adorner;
+                    UpdatePointAndSize();
+
+                    foreach (var item in MyEzShapeAdorner.MyAnchorThumbsList)
+                    {
+                        item.DragDelta += EzShapeAnchor_DragDelta;
+                        item.DragCompleted += EzShapeAnchor_DragCompleted;
+                    }
+                }
+                //在れば削除
+                else
+                {
+                    layer.Remove(MyEzShapeAdorner);
+                    MyEzShapeAdorner = null;
+                    UpdatePointAndSize();
+                }
+            }
+        }
+
+        private void EzShapeAnchor_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            UpdatePointAndSize();
+        }
+
+        /// <summary>
+        /// アンカーのドラッグ移動時の処理
+        /// 対応Pointを更新、アンカーの移動
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EzShapeAnchor_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is Thumb t && MyEzShapeAdorner != null)
+            {
+                int id = (int)t.Tag;
+                Point po = MyPoints[id];
+                double left = po.X + e.HorizontalChange;
+                double top = po.Y + e.VerticalChange;
+
+                //Pointの更新
+                MyPoints[id] = new(left, top);
+
+                //アンカーの移動
+                Canvas.SetLeft(t, left - MyEzShapeAdorner.AnchorSize / 2.0);
+                Canvas.SetTop(t, top - MyEzShapeAdorner.AnchorSize / 2.0);
+
+                e.Handled = true;
+            }
+        }
+
+        public void FixAdornerLocate()
+        {
+            MyEzShapeAdorner?.ResetAnchorLocate();
+        }
+
+
+
+
+
+        /// <summary>
+        /// Points全体のリセット、左上に寄せる、TopLeftを0にする
+        /// </summary>
+        public void FixPointsZero()
+        {
+            var (left, top) = GetTopLeftFromPoints();
+            for (int i = 0; i < MyPoints.Count; i++)
+            {
+                Point p = MyPoints[i];
+                MyPoints[i] = new Point(p.X - left, p.Y - top);
+            }
+        }
+        public void FixPointsZero(double offsetX, double offsetY)
+        {
+            for (int i = 0; i < MyPoints.Count; i++)
+            {
+                Point p = MyPoints[i];
+                MyPoints[i] = new Point(p.X - offsetX, p.Y - offsetY);
+            }
+        }
+
+        private (double left, double top) GetTopLeftFromPoints()
+        {
+            double left = double.MaxValue;
+            double top = double.MaxValue;
+            foreach (var item in MyPoints)
+            {
+                if (left > item.X) { left = item.X; }
+                if (top > item.Y) { top = item.Y; }
+            }
+            return (left, top);
+        }
+
+        /// <summary>
+        /// Pointsとアンカーポイントが収まるRect
+        /// </summary>
+        /// <returns></returns>
+        private Rect GetBoundsFromPointsAndAnchorThumb()
+        {
+            Rect r = new();
+            double left = double.MaxValue; double top = double.MaxValue;
+            foreach (var item in MyPoints)
+            {
+                if (left > item.X) { left = item.X; }
+                if (top > item.Y) { top = item.Y; }
+            }
+
+            PointCollection pc = [];
+            foreach (var item in MyPoints)
+            {
+                pc.Add(new Point(item.X - left, item.Y - top));
+            }
+
+            foreach (var item in pc)
+            {
+                Rect pr = new(item.X - 10, item.Y - 10, 20, 20);
+                r.Union(pr);
+            }
+            return r;
+        }
+
+        /// <summary>
+        /// 全部のアンカーポイントが収まるRectを返す
+        /// アンカーポイントが表示されていなければ0を返す
+        /// </summary>
+        /// <returns></returns>
+        private Rect GetBoundsFromAnchorThumb()
+        {
+            if (MyEzShapeAdorner != null)
+            {
+                double anchorSize = MyEzShapeAdorner.AnchorSize;
+                double anchorSizeHalf = anchorSize / 2.0;//アンカーポイントの中心位置
+                Rect r = new();
+                foreach (var item in MyPoints)
+                {
+                    Rect pr = new(item.X - anchorSizeHalf, item.Y - anchorSizeHalf, anchorSize, anchorSize);
+                    r.Union(pr);
+                }
+                return r;
+            }
+            else
+            {
+                return new Rect();
+            }
+
+        }
+
+
+    }
+
+
+    public class EzBezierThumb : EzShapeThumb
+    {
+        static EzBezierThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EzBezierThumb), new FrameworkPropertyMetadata(typeof(EzBezierThumb)));
+        }
+        public EzBezierThumb()
+        {
+
+        }
+    }
+
+
+    public class Waku : Control
+    {
+        static Waku()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Waku), new FrameworkPropertyMetadata(typeof(Waku)));
+        }
+        public Waku()
+        {
+
+        }
+    }
+
+
+}
---
+++ 20250308/TwoColorDashLine.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace _20250308
+{
+    //[028722]ベジエ曲線の各部の名称
+    //https://support.justsystems.com/faq/1032/app/servlet/qadoc?QID=028722
+
+    //ベジェ曲線の方向線とアンカーポイント、制御点を表示してみた - 午後わてんのブログ
+    //https://gogowaten.hatenablog.com/entry/15547295
+    //WPF、ベジェ曲線で直線表示、アンカー点の追加と削除 - 午後わてんのブログ
+    //https://gogowaten.hatenablog.com/entry/2022/06/14/132217
+
+    /// <summary>
+    /// ベジェ曲線の方向線表示用、2色破線
+    /// OnRenderで直線描画、その上にDefiningGeometryで破線描画
+    /// </summary>
+
+    class TwoColorDashLine : Shape
+    {
+        public TwoColorDashLine()
+        {
+            Stroke = Brushes.White;
+            StrokeThickness = 2.0;
+            StrokeDashArray = [5.0];
+            SetMyBind();
+            Loaded += TwoColorDashLine_Loaded;
+        }
+
+        private void TwoColorDashLine_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void SetMyBind()
+        {
+            var mb = new MultiBinding() {Converter = new MyConvLinePen() };
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(MyStrokeBaseProperty) });
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(StrokeThicknessProperty) });
+            SetBinding(MyPenProperty, mb);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            for (int i = 0; i < MyPoints.Count-1; i++)
+            {
+                if ((i - 1) % 3 != 0)
+                {
+                    drawingContext.DrawLine(MyPen, MyPoints[i], MyPoints[i + 1]);
+                }
+            }
+            base.OnRender(drawingContext);
+        }
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                if (MyPoints == null) { return Geometry.Empty; }
+                StreamGeometry geo = new();
+                using var context = geo.Open();
+                for (int i = 0; i < MyPoints.Count-1; i++)
+                {
+                    if ((i - 1) % 3 != 0)
+                    {
+                        context.BeginFigure(MyPoints[i], isFilled: false, isClosed: false);
+                        context.LineTo(MyPoints[i + 1], isStroked: true, isSmoothJoin: false);
+                    }
+                }
+                geo.Freeze();
+                return geo;
+            }
+        }
+
+
+        #region 依存関係プロパティ
+
+
+        public Pen MyPen
+        {
+            get { return (Pen)GetValue(MyPenProperty); }
+            set { SetValue(MyPenProperty, value); }
+        }
+        public static readonly DependencyProperty MyPenProperty =
+            DependencyProperty.Register(nameof(MyPen), typeof(Pen), typeof(TwoColorDashLine), new PropertyMetadata(null));
+
+
+        public PointCollection MyPoints
+        {
+            get { return (PointCollection)GetValue(MyPointsProperty); }
+            set { SetValue(MyPointsProperty, value); }
+        }
+        public static readonly DependencyProperty MyPointsProperty =
+            DependencyProperty.Register(nameof(MyPoints), typeof(PointCollection), typeof(TwoColorDashLine),
+                new FrameworkPropertyMetadata(null,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+        public SolidColorBrush MyStrokeBase
+        {
+            get { return (SolidColorBrush)GetValue(MyStrokeBaseProperty); }
+            set { SetValue(MyStrokeBaseProperty, value); }
+        }
+        public static readonly DependencyProperty MyStrokeBaseProperty =
+            DependencyProperty.Register(nameof(MyStrokeBase), typeof(SolidColorBrush), typeof(TwoColorDashLine), new PropertyMetadata(Brushes.Black));
+        #endregion 依存関係プロパティ
+
+    }
+
+
+    #region コンバーター
+    public class MyConvLinePen : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var stroke = (SolidColorBrush)values[0];
+            var thickness = (double)values[1];
+            return new Pen(stroke, thickness);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion コンバーター
+}
