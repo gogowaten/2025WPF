25ab18592a399574991e8812ea758deeaeab8544
waten <gogowaten@yahoo.co.jp>
2025-03-15 13:04:50 +0900

    202503151304

---
+++ 20250315_ResizeAdorner/CustomControl1.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace _20250315_ResizeAdorner
+{
+    public class HandleThumb : Thumb
+    {
+        static HandleThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HandleThumb), new FrameworkPropertyMetadata(typeof(HandleThumb)));
+        }
+        public HandleThumb()
+        {
+
+        }
+
+        //Canvas.Leftとバインドする用
+        public double MyLeft
+        {
+            get { return (double)GetValue(MyLeftProperty); }
+            set { SetValue(MyLeftProperty, value); }
+        }
+        public static readonly DependencyProperty MyLeftProperty =
+            DependencyProperty.Register(nameof(MyLeft), typeof(double), typeof(HandleThumb),
+                new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double MyTop
+        {
+            get { return (double)GetValue(MyTopProperty); }
+            set { SetValue(MyTopProperty, value); }
+        }
+        public static readonly DependencyProperty MyTopProperty =
+            DependencyProperty.Register(nameof(MyTop), typeof(double), typeof(HandleThumb),
+                new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    }
+}
---
+++ 20250315_ResizeAdorner/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace _20250315_ResizeAdorner;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MyButtonChangeHandleVisible_Click(object sender, RoutedEventArgs e)
+    {
+        ResizeAdorner? adorner = SwitchAdorner(MyTarget);
+        MyStackPanel.DataContext = adorner;
+        _ = SwitchAdorner(MyButton);
+        _ = SwitchAdorner(MyButtonInStackPanel);
+        _ = SwitchAdorner(MyEllipse);
+    }
+
+    /// <summary>
+    /// 対象にリサイズ用のハンドル(装飾)を付け外しする
+    /// </summary>
+    /// <param name="elem">対象要素</param>
+    /// <returns>装飾</returns>
+    private static ResizeAdorner? SwitchAdorner(FrameworkElement elem)
+    {
+        if (AdornerLayer.GetAdornerLayer(elem) is AdornerLayer layer)
+        {
+            var items = layer.GetAdorners(elem);
+            if (items != null)
+            {
+                foreach (var item in items.OfType<ResizeAdorner>())
+                {
+                    layer.Remove(item);
+                }
+                return null;
+            }
+            else
+            {
+                var adorner = new ResizeAdorner(elem);
+                layer.Add(adorner);
+                return adorner;
+            }
+        }
+        return null;
+    }
+
+    private void MyButtonChangeBackground_Click(object sender, RoutedEventArgs e)
+    {
+        if (MyGrid.Background == null || MyGrid.Background == Brushes.White)
+        {
+            MyGrid.Background = Brushes.Black;
+        }
+        else
+        {
+            MyGrid.Background = Brushes.White;
+        }
+    }
+
+}
