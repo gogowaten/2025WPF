fdc207fb16c8856308c71cb376681b40bbbd9c3f
waten <gogowaten@yahoo.co.jp>
2025-03-16 15:21:53 +0900

    202503161521

---
+++ 20250315/ItemData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20250315
+{
+    public class ItemData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void SetProperty<T>(ref T field, T value, [System.Runtime.CompilerServices.CallerMemberName] string? name = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return;
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ItemData()
+        {
+
+        }
+
+
+        private double _myStrokeThickness = 30.0;
+        public double MyStrokeThickness { get => _myStrokeThickness; set => SetProperty(ref _myStrokeThickness, value); }
+
+    }
+}
---
+++ 20250315/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20250315;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private GeoShapeTThumb? MyGeoShapeTThumb { get; set; }
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (MyGeoShapeTThumb != null)
+        {
+            _ = SwitchAdorner(MyGeoShapeTThumb);
+        }
+    }
+
+
+    /// <summary>
+    /// 対象にリサイズ用のハンドル(装飾)を付け外しする
+    /// </summary>
+    /// <param name="elem">対象要素</param>
+    /// <returns>装飾</returns>
+    private static ResizeHandleAdorner? SwitchAdorner(FrameworkElement elem)
+    {
+        if (AdornerLayer.GetAdornerLayer(elem) is AdornerLayer layer)
+        {
+            var items = layer.GetAdorners(elem);
+            if (items != null)
+            {
+                foreach (var item in items.OfType<ResizeHandleAdorner>())
+                {
+                    layer.Remove(item);
+                }
+                return null;
+            }
+            else
+            {
+                var adorner = new ResizeHandleAdorner(elem);
+                layer.Add(adorner);
+                return adorner;
+            }
+        }
+        return null;
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        var data = new ItemData();
+        data.MyStrokeThickness = 40.0;
+        MyGeoShapeTThumb = new(data);
+        MyCanvas.Children.Add(MyGeoShapeTThumb);
+        DataContext = MyGeoShapeTThumb;
+    }
+}
