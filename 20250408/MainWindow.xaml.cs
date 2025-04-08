140f0efc169166b554d41a0ae35c889e3a79ab61
waten <gogowaten@yahoo.co.jp>
2025-04-08 13:14:52 +0900

    202504081314

---
+++ 20250408/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20250408
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Test1()
+        {
+            MyShape0.AnchorSwitch();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Test1();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MyShape0.MyPoints[1] = new Point(300, 100);
+        }
+    }
+}
