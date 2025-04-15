a97007e52f5cd21841c9857918ce23464072ce01
waten <gogowaten@yahoo.co.jp>
2025-04-16 00:14:09 +0900

    202504160014

--- 20250415/MainWindow.xaml.cs
+++ 20250415/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.DirectoryServices.ActiveDirectory;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -30,35 +31,51 @@ namespace _20250415
 
         private void Bai_Click(object sender, RoutedEventArgs e)
         {
-            double x = MyGeo.RenderedGeometry.Bounds.Width / 2.0;
-            double y = MyGeo.RenderedGeometry.Bounds.Height / 2.0;
-            ScaleTransform sc = new(2, 2,x,y);
-            for (int i = 0; i < MyGeo.MyPoints.Count; i++)
-            {
-                Point poi = MyGeo.MyPoints[i];
-                poi = sc.Transform(poi);
-                MyGeo.MyPoints[i] = poi;
-            }
+            var (x, y) = GetCenterXY(MyGeo.RenderedGeometry, MyGeo.MyPen);
+            MyPointsTransform(new ScaleTransform(2, 2, x, y));
         }
 
         private void Half_Click(object sender, RoutedEventArgs e)
         {
-            ScaleTransform sc = new(0.5, 0.5);
-            for (int i = 0; i < MyGeo.MyPoints.Count; i++)
-            {
-                //var poi = MyGeo.MyPoints[i];
-                //poi = sc.Transform(poi);
-                //MyGeo.MyPoints[i] = poi;
-                MyGeo.MyPoints[i]=sc.Transform(MyGeo.MyPoints[i]);
-            }
+            MyPointsTransform(new ScaleTransform(0.5, 0.5));
         }
 
         private void GetBounds_Click(object sender, RoutedEventArgs e)
         {
-           var geo = MyGeo.RenderedGeometry;
-           var geo2= MyGeo.MyGeometry;
-           var bounds = geo.GetRenderBounds(MyGeo.MyPen);
+            var geo = MyGeo.RenderedGeometry;
+            var geo2 = MyGeo.MyGeometry;
+            var bounds = MyGeo.RenderedGeometry.GetRenderBounds(MyGeo.MyPen);
 
         }
+
+        private void Right_Click(object sender, RoutedEventArgs e)
+        {
+            var (x, y) = GetCenterXY(MyGeo.RenderedGeometry, MyGeo.MyPen);
+            MyPointsTransform(new RotateTransform(15, x, y));
+        }
+
+        /// <summary>
+        /// 中心座標
+        /// </summary>
+        /// <param name="geo"></param>
+        /// <param name="pen"></param>
+        /// <returns></returns>
+        private static (double x, double y) GetCenterXY(Geometry geo, Pen pen)
+        {
+            var penBounds = geo.GetRenderBounds(pen);
+            double x = penBounds.Width / 2.0 + penBounds.Left;
+            double y = penBounds.Height / 2.0 + penBounds.Top;
+            return (x, y);
+        }
+
+        private void MyPointsTransform(Transform transform)
+        {
+            for (int i = 0; i < MyGeo.MyPoints.Count; i++)
+            {
+                MyGeo.MyPoints[i] = transform.Transform(MyGeo.MyPoints[i]);
+            }
+        }
+
+
     }
 }
