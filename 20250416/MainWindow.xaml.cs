88b0013cbd7469e94d5b23bdc59c74bc3a121486
waten <gogowaten@yahoo.co.jp>
2025-04-16 21:37:59 +0900

    202504162137

---
+++ 20250416/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20250416
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KisoThumb MyThumb { get; set; } = null!;
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyThumb = Test1();
+            DataContext = MyThumb;
+
+
+        }
+
+        private TextBlockThumb Test1()
+        {
+            ItemData data = new();
+            data.MyText = "test";
+            data.MyLeft = 100;
+            data.MyTop = 100;
+            var thumb = new TextBlockThumb(data);
+            MyCanvas.Children.Add(thumb);
+            return thumb;
+        }
+    }
+}
