66df55dacd42f20464bf75ee8bfc925f19594b04
waten <gogowaten@yahoo.co.jp>
2025-04-22 22:24:23 +0900

    202504222224

---
+++ 20250422/ExCanvas.cs
@@ -0,0 +1,202 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace _20250422
+{
+    public class ManageExCanvas : ExCanvas
+    {
+        public RootThumb MyRootThumb { get; set; }
+        public AreaThumb MyAreaThumb { get; set; }
+        private ContextMenu MyContextMenuForAreaThumb { get; set; } = new();
+
+        //public ManageExCanvas() { }
+        public ManageExCanvas(RootThumb rootThumb)
+        {
+            MyRootThumb = rootThumb;
+            MyAreaThumb = new();
+            Children.Add(MyRootThumb);
+            Children.Add(MyAreaThumb);
+
+            MyAreaThumb.Width = 100;
+            MyAreaThumb.Height = 100;
+            MyAreaThumb.Opacity = 0.5;
+            MyAreaThumb.Background = Brushes.Red;
+            InitializeContextMenu();
+            MyAreaThumb.ContextMenu = MyContextMenuForAreaThumb;
+        }
+
+
+        private void InitializeContextMenu()
+        {
+            MenuItem item;
+            item = new() { Header = "範囲を画像として保存" };
+            MyContextMenuForAreaThumb.Items.Add(item);
+            item.Click += Item_Click;
+        }
+
+        private void Item_Click(object sender, RoutedEventArgs e)
+        {
+            var bmp = GetAreaBitmap();
+            MyRootThumb.SaveBitmap(bmp, 90);
+        }
+
+
+        //AreaThumb用、選択範囲の画像作成
+        private RenderTargetBitmap GetAreaBitmap()
+        {
+            //描画のRect取得
+            var bounds = MyAreaThumb.TransformToVisual(MyRootThumb)
+                .TransformBounds(VisualTreeHelper.GetDescendantBounds(MyAreaThumb));
+            DrawingVisual dv = new() { Offset = new Vector(-bounds.X, -bounds.Y) };
+            using (var context = dv.RenderOpen())
+            {
+                VisualBrush vb = new(MyRootThumb) { Stretch = Stretch.None };
+                context.DrawRectangle(vb, null, VisualTreeHelper.GetDescendantBounds(MyRootThumb));
+            }
+            RenderTargetBitmap bitmap = new(
+                (int)Math.Ceiling(bounds.Width), (int)Math.Ceiling(bounds.Height)
+                , 96, 96, PixelFormats.Pbgra32);
+            bitmap.Render(dv);
+            return bitmap;
+        }
+
+        #region 右クリックメニュー
+        private ContextMenu MakeContext()
+        {
+            ContextMenu menu = new();
+            MenuItem item;
+            item = new() { Header = "グループ化" }; menu.Items.Add(item);
+            //item.Click += (s, e) => { MyRoot.AddGroup(); };
+            //item = new() { Header = "画像で複製" }; menu.Items.Add(item);
+            //item.Click += (s, e) => { MyRoot.DuplicateImageSelectedThumbs(); };
+            //item = new() { Header = "Dataで複製" }; menu.Items.Add(item);
+            //item.Click += (s, e) => { MyRoot.DuplicateDataSelectedThumbs(); };
+
+            //menu.Items.Add(new Separator() { Height = 10 });
+            //item = new() { Header = "削除" }; menu.Items.Add(item);
+            //item.Click += (s, e) => { MyRoot.RemoveThumb(); };
+            //menu.Items.Add(new Separator() { Height = 10 });
+
+            ////item = new() { Header = "Z移動" };
+            //menu.Items.Add(MakeMenuItemZMove());
+
+            //item = new() { Header = "複数選択解除" }; menu.Items.Add(item);
+            //item.Click += (s, e) => { throw new ArgumentException(); };
+
+            return menu;
+        }
+
+
+        #endregion 右クリックメニュー
+
+        #region 画像保存
+
+
+
+
+        //public bool SaveBitmap2(BitmapSource bitmap)
+        //{
+        //    Microsoft.Win32.SaveFileDialog dialog = new()
+        //    {
+        //        Filter = "*.png|*.png|*.jpg|*.jpg;*.jpeg|*.bmp|*.bmp|*.gif|*.gif|*.tiff|*.tiff",
+        //        AddExtension = true,
+        //    };
+        //    if (dialog.ShowDialog() == true)
+        //    {
+        //        (BitmapEncoder? encoder, BitmapMetadata? meta) = GetEncoderWithMetaData(dialog.FilterIndex);
+        //        if (encoder is null) { return false; }
+        //        encoder.Frames.Add(BitmapFrame.Create(bitmap, null, meta, null));
+        //        try
+        //        {
+        //            using FileStream stream = new(dialog.FileName, FileMode.Create, FileAccess.Write);
+        //            encoder.Save(stream);
+        //            return true;
+        //        }
+        //        catch (Exception)
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    return false;
+        //}
+
+        #endregion 画像保存
+    }
+
+
+
+    /// <summary>
+    /// 子要素全体が収まるようにサイズが自動変化するCanvas
+    /// ただし、子要素のマージンとパディングは考慮していないし
+    /// ArrangeOverrideを理解していないので不具合があるかも
+    /// </summary>
+    public class ExCanvas : Canvas
+    {
+        private bool isAutoResize = true;
+
+        /// <summary>
+        /// 自動リサイズの切り替えフラグ
+        /// falseからtrueに変更時はInvalidateArrangeを実行してリサイズ
+        /// </summary>
+        public bool IsAutoResize
+        {
+            get => isAutoResize;
+            set
+            {
+                if (isAutoResize != value)
+                {
+                    isAutoResize = value;
+                    if (value) { InvalidateArrange(); }
+                }
+            }
+        }
+
+        public ExCanvas()
+        {
+            Loaded += ExCanvas_Loaded;
+        }
+
+        private void ExCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetBinding(WidthProperty, new Binding() { Source = this, Path = new PropertyPath(ActualWidthProperty) });
+            SetBinding(HeightProperty, new Binding() { Source = this, Path = new PropertyPath(ActualHeightProperty) });
+        }
+
+        protected override Size ArrangeOverride(Size arrangeSize)
+        {
+            //if (double.IsNaN(Width) && double.IsNaN(Height) && IsAutoResize)
+            if (IsAutoResize)
+            {
+                base.ArrangeOverride(arrangeSize);
+                Size resultSize = new();
+                foreach (var item in Children.OfType<FrameworkElement>())
+                {
+
+                    double x = GetLeft(item) + item.ActualWidth;
+                    if (double.IsNaN(x)) { x = 0 + item.ActualWidth; }
+                    double y = GetTop(item) + item.ActualHeight;
+                    if (double.IsNaN(y)) { y = 0 + item.ActualHeight; }
+                    if (resultSize.Width < x) resultSize.Width = x;
+                    if (resultSize.Height < y) resultSize.Height = y;
+                }
+                //base.ArrangeOverride(resultSize);
+                return resultSize;
+            }
+            else
+            {
+                return base.ArrangeOverride(arrangeSize);
+            }
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            return base.MeasureOverride(constraint);
+        }
+    }
+
+
+}
---
+++ 20250422/MainWindow.xaml.cs
@@ -0,0 +1,405 @@
+﻿using Microsoft.Win32;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Point = System.Windows.Point;
+
+
+//2025WPF / 20250421 at main · gogowaten/2025WPF
+//https://github.com/gogowaten/2025WPF/tree/main/20250421
+//を改変
+
+namespace _20250422;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    //private ContextMenu MyContextMenu = null!;
+    private RootThumb MyRoot { get; set; } = null!;
+    private ManageExCanvas MyManageExCanvas { get; set; }=null!;
+
+    string SaveFileName = "E:\\" + DateTime.Now.ToString("yyyyMMdd") + ".xml";
+    string SaveRootFileName = "E:\\" + DateTime.Now.ToString("yyyyMMdd") + "Root.xml";
+    string SaveFileNameZip = "E:\\" + DateTime.Now.ToString("yyyyMMdd") + ".zip";
+    string SaveRootFileNameZip = "E:\\" + DateTime.Now.ToString("yyyyMMdd") + "Root.zip";
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        MyInitialize();
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        //SwitchAdorner(MyRange);
+
+    }
+
+    #region 初期処理
+    private void MyInitialize()
+    {
+        AllowDrop = true;
+        Drop += MainWindow_Drop;
+
+        ItemData data = new(ThumbType.Root) { MyBackground = Brushes.SkyBlue };
+        //if (new RootThumb(data) is RootThumb root)
+        //{
+        //    MyRoot = root;
+        //}
+        MyRoot = new RootThumb(data);
+        //MyScrollViewer.Content = MyRoot;
+        //MyPanel.Children.Add(MyRoot);
+        MyManageExCanvas = new(MyRoot);
+        MyScrollViewer.Content = MyManageExCanvas;
+        DataContext = MyRoot;
+
+    }
+
+
+    private void MainWindow_Drop(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            MyRoot.OpenFiles(paths);
+        }
+    }
+
+
+
+    #endregion 初期処理
+
+
+
+
+    #region メソッド
+
+
+    /// <summary>
+    /// 対象にリサイズ用のハンドル(装飾)を付け外しする
+    /// </summary>
+    /// <param name="elem">対象要素</param>
+    /// <returns>装飾</returns>
+    private static ResizeHandleAdorner? SwitchAdorner(FrameworkElement elem)
+    {
+        if (AdornerLayer.GetAdornerLayer(elem) is AdornerLayer layer)
+        {
+            var items = layer.GetAdorners(elem);
+            if (items != null)
+            {
+                foreach (var item in items.OfType<ResizeHandleAdorner>())
+                {
+                    layer.Remove(item);
+                }
+                return null;
+            }
+            else
+            {
+                var adorner = new ResizeHandleAdorner(elem);
+                adorner.MyHandleLayout = HandleLayoutType.Inside;
+                //adorner.MyHandleLayout = HandleLayoutType.Online;
+                layer.Add(adorner);
+                return adorner;
+            }
+        }
+        return null;
+    }
+
+
+    #endregion メソッド
+
+    private void MyTest2_Click(object sender, RoutedEventArgs e)
+    {
+        //ItemData data = new(ThumbType.Image);
+        //var bmp = MyRoot.GetBitmap("D:\\ブログ用\\テスト用画像\\hueRectT000.png");
+        //data.MyBitmapSource = bmp;
+        //MyRoot.AddNewThumbFromItemData(data, MyRoot);
+
+
+    }
+
+    private void AddTextThumb()
+    {
+        var data = new ItemData(ThumbType.Text)
+        {
+            MyText = "TextBlock",
+            MyFontSize = 30,
+            MyForeground = Brushes.RosyBrown,
+            MyBackground = Brushes.SeaShell,
+        };
+        MyRoot.AddNewThumbFromItemData(data, MyRoot.MyActiveGroupThumb);
+    }
+    private void AddEllipseTextThumb()
+    {
+        var data = new ItemData(ThumbType.Ellipse)
+        {
+            MyText = "Ellipse",
+            MyFontSize = 30,
+            MyForeground = Brushes.LightCoral,
+            MyFill = Brushes.MistyRose,
+            MyWidth = 80,
+            MyHeight = 80
+        };
+        MyRoot.AddNewThumbFromItemData(data, MyRoot.MyActiveGroupThumb);
+    }
+
+
+    private void AddTextThumb_Click(object sender, RoutedEventArgs e)
+    {
+        AddTextThumb();
+    }
+
+    private void AddEllipseThumb_Click(object sender, RoutedEventArgs e)
+    {
+        AddEllipseTextThumb();
+    }
+
+    //FocusThumbをファイルに保存
+    private void SaveToFile_Click(object sender, RoutedEventArgs e)
+    {
+        //_ = MyRoot.MyFocusThumb?.MyItemData.Serialize(SaveFileName);
+        if (MyRoot.MyFocusThumb?.MyItemData is ItemData data)
+        {
+            //if (MyRoot.SaveItemData(data, "E:\\20250329ItemData.zip"))
+            if (MyRoot.SaveItemData(data, SaveFileNameZip))
+            {
+                MessageBox.Show("保存した");
+            }
+        }
+    }
+
+    //ファイルからの読み込み
+    private void LoadFile_Click(object sender, RoutedEventArgs e)
+    {
+        //if (ItemData.Deserialize(SaveFileName) is ItemData data)
+        //{
+        //    //ファイルから追加するときは0座標にしないと離れた位置に追加される
+        //    data.MyLeft = 0;
+        //    data.MyTop = 0;
+        //    MyRoot.AddNewThumbFromItemData(data, MyRoot.MyActiveGroupThumb);
+        //}
+        if (MyRoot.LoadItemData(SaveFileNameZip) is ItemData data)
+        {
+            data.MyLeft = 0; data.MyTop = 0;
+            MyRoot.AddNewThumbFromItemData(data);
+        }
+    }
+
+    private void AddGroup_Click(object sender, RoutedEventArgs e)
+    {
+        MyRoot.AddGroupFromSelected();
+    }
+
+    private void Ungroup_Click(object sender, RoutedEventArgs e)
+    {
+        MyRoot.UngroupFocusThumb();
+    }
+
+    private void Remove_Click(object sender, RoutedEventArgs e)
+    {
+        //MyRoot.RemoveThumb(MyRoot.MyFocusThumb);
+        MyRoot.RemoveThumb();
+    }
+
+    private void WakuVisible_Click(object sender, RoutedEventArgs e)
+    {
+
+        if (MyRoot.IsWakuVisible == Visibility.Visible)
+        {
+            MyRoot.IsWakuVisible = Visibility.Collapsed;
+        }
+        else
+        {
+            MyRoot.IsWakuVisible = Visibility.Visible;
+        }
+        //MyRoot.WakuVisible(false);
+    }
+
+    private void In_Click(object sender, RoutedEventArgs e)
+    {
+        MyRoot.ActiveGroupToInside();
+    }
+
+    private void Out_Click(object sender, RoutedEventArgs e)
+    {
+        MyRoot.ActiveGroupToOutside();
+    }
+
+    private void Up_Click(object sender, RoutedEventArgs e)
+    {
+        MyRoot.MyFocusThumb?.ZIndexUp();
+    }
+
+    private void Down_Click(object sender, RoutedEventArgs e)
+    {
+        MyRoot.MyFocusThumb?.ZIndexDown();
+    }
+
+    private void Top_Click(object sender, RoutedEventArgs e)
+    {
+        MyRoot.MyFocusThumb?.ZIndexTop();
+    }
+
+    private void Bottom_Click(object sender, RoutedEventArgs e)
+    {
+        MyRoot.MyFocusThumb?.ZIndexBottom();
+    }
+
+    //Rootの保存、全体の保存
+    private void SaveRoot_Click(object sender, RoutedEventArgs e)
+    {
+        //_ = MyRoot.MyItemData.Serialize(SaveRootFileName);
+        MyRoot.SaveItemData(MyRoot.MyItemData, SaveRootFileNameZip);
+    }
+
+    //Rootファイルの読み込み
+    private void ReadRootFile_Click(object sender, RoutedEventArgs e)
+    {
+        ////画像なしDataのとき
+        //if (ItemData.Deserialize(SaveRootFileName) is ItemData data)
+        //{
+        //    if (new RootThumb(data) is RootThumb root)
+        //    {
+        //        MyScrollViewer.Content = root;
+        //        MyRoot = root;
+        //        DataContext = root;
+        //    }
+        //}
+        if (MyRoot.LoadItemData(SaveRootFileNameZip) is ItemData data)
+        {
+            if (new RootThumb(data) is RootThumb root)
+            {
+                MyScrollViewer.Content = root;
+                MyRoot = root;
+                DataContext = root;
+            }
+        }
+    }
+
+    private void RemoveAll_Click(object sender, RoutedEventArgs e)
+    {
+        MyRoot.RemoveAll();
+    }
+
+
+    private void AnchorSwitch_Click(object sender, RoutedEventArgs e)
+    {
+        //if (MyRoot.MyFocusThumb is EzBezierThumb bezi)
+        //{
+        //    bezi.AnchorOnOffSwitch();
+        //}
+        if (MyRoot.MyFocusThumb is GeoShapeThumb2 shape)
+        {
+            shape.AnchorSwitch();
+        }
+    }
+
+    private void AddPolyLine_Click(object sender, RoutedEventArgs e)
+    {
+        var data = new ItemData(ThumbType.GeoShape)
+        {
+            MyText = "図形",
+            MyPoints = [new Point(), new Point(100, 0), new Point(100, 100), new Point(0, 100)],
+            MyBackground = null,
+            MyStroke = Brushes.Khaki,
+            MyStrokeThickness = 30.0,
+        };
+        MyRoot.AddNewThumbFromItemData(data, MyRoot.MyActiveGroupThumb);
+    }
+
+    private void AddBezier_Click(object sender, RoutedEventArgs e)
+    {
+        var data = new ItemData(ThumbType.GeoShape)
+        {
+            MyText = "図形",
+            MyPoints = [new Point(), new Point(200, 0), new Point(0, 100), new Point(200, 100)],
+            MyBackground = null,
+            MyStroke = Brushes.PaleGoldenrod,
+            MyStrokeThickness = 30.0,
+            MyShapeType = ShapeType.Bezier,
+            MyGeoShapeHeadCapType = HeadType.Arrow,
+        };
+        MyRoot.AddNewThumbFromItemData(data, MyRoot.MyActiveGroupThumb);
+    }
+
+    private void AddPoint_Click(object sender, RoutedEventArgs e)
+    {
+        if (MyRoot.MyFocusThumb is GeoShapeThumb2 geo2)
+        {
+            Random r = new();
+            //Point maeP = geo2.MyItemData.MyPoints[^1];
+
+            //geo2.MyItemData.MyPoints.Add(new Point(100, 140));
+            geo2.AddPoint(new Point(r.Next(200), r.Next(200)));
+
+        }
+
+    }
+
+    private void RemovePoint_Click(object sender, RoutedEventArgs e)
+    {
+        if (MyRoot.MyFocusThumb is GeoShapeThumb2 geo2)
+        {
+            geo2.RemovePoint();
+        }
+    }
+
+    private void ShapeTypeSwitch_Click(object sender, RoutedEventArgs e)
+    {
+        if (MyRoot.MyFocusThumb is GeoShapeThumb2 geo2)
+        {
+            geo2.ShapeTypeSwitch();
+            var neko = geo2.MyItemData.MyBackground;
+        }
+    }
+
+    private void ShapeCapSwitch_Click(object sender, RoutedEventArgs e)
+    {
+        if (MyRoot.MyFocusThumb is GeoShapeThumb2 shape)
+        {
+            HeadType type = shape.MyItemData.MyGeoShapeHeadCapType;
+            if (type == HeadType.None)
+            {
+                shape.MyItemData.MyGeoShapeHeadCapType = HeadType.Arrow;
+            }
+            else
+            {
+                shape.MyItemData.MyGeoShapeHeadCapType = HeadType.None;
+            }
+            shape.UpdateLocateAndSize();
+            MyRoot.MyFocusThumb.MyParentThumb?.ReLayout3();
+        }
+    }
+
+    //画像ファイルから追加、複数対応、未対応ファイルの場合は追加できなかったものをメッセージ表示
+    private void AddImageFiles_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog dialog = new() { Multiselect = true };
+
+        if (dialog.ShowDialog() is true)
+        {
+            MyRoot.OpenFiles(dialog.FileNames);
+        }
+    }
+
+    private void SaveToImageFile_Click(object sender, RoutedEventArgs e)
+    {
+        //FocusThumbを画像として保存
+        MyRoot.SaveMyFocusThumbToImageFile();
+    }
+
+}
