d1ad17c1b4daf2d2b078a6a0cc862ea61bc190b9
waten <gogowaten@yahoo.co.jp>
2025-05-16 13:40:16 +0900

    202505161340

---
+++ 20250516/InputBox.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _20250516
+{
+    /// <summary>
+    /// InputBox.xaml の相互作用ロジック
+    /// </summary>
+    public partial class InputBox : Window
+    {
+        public InputBox(string text)
+        {
+            InitializeComponent();
+            MyTextBox.Text = text;
+            MyTextBox.SelectAll();
+        }
+
+        private void MyOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void MyCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+    }
+}
---
+++ 20250516/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20250516
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OpenDialog_Click(object sender, RoutedEventArgs e)
+        {
+            InputBox inputBox = new(MyTextBlock.Text);
+            bool? result = inputBox.ShowDialog();
+            if (result == true)
+            {
+                MyTextBlock.Text = inputBox.MyTextBox.Text;
+            }
+        }
+    }
+}
