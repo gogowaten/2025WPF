dd726c009f75a0a75856e120e59e075377e031ab
waten <gogowaten@yahoo.co.jp>
2025-05-16 18:16:28 +0900

    202505161816

--- 20250516/MainWindow.xaml.cs
+++ 20250516/MainWindow.xaml.cs
@@ -1,33 +1,25 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
+//ダイアログ ボックスの概要 - WPF | Microsoft Learn
+//https://learn.microsoft.com/ja-jp/dotnet/desktop/wpf/windows/dialog-boxes-overview?view=netdesktop-9.0
+
+// ウィンドウを閉じる方法 - WPF | Microsoft Learn
+//https://learn.microsoft.com/ja-jp/dotnet/desktop/wpf/windows/how-to-close-window-dialog-box
 
 namespace _20250516
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
 
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
         private void OpenDialog_Click(object sender, RoutedEventArgs e)
         {
             InputBox inputBox = new(MyTextBlock.Text);
-            bool? result = inputBox.ShowDialog();
-            if (result == true)
+            if (inputBox.ShowDialog() == true)
             {
                 MyTextBlock.Text = inputBox.MyTextBox.Text;
             }
