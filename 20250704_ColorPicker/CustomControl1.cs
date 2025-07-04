4e9473bd1486ae1513dd20bc7bb80128b7f0e7af
waten <gogowaten@yahoo.co.jp>
2025-07-04 22:22:26 +0900

    202507042222

---
+++ 20250704_ColorPicker/CustomControl1.cs
@@ -0,0 +1,250 @@
+﻿using _20250704_ColorPicker;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _20250704_ColorPicker
+{
+
+
+
+
+
+
+    public class Pikka : Control
+    {
+        static Pikka()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Pikka), new FrameworkPropertyMetadata(typeof(Pikka)));
+        }
+
+        public Pikka()
+        {
+
+        }
+
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (GetTemplateChild("PART_Viewbox") is Viewbox vb)
+            {
+                if (AdornerLayer.GetAdornerLayer(vb) is AdornerLayer layer)
+                {
+                    MyMarkerAdorner = new MarkerAdorner(vb);
+                    layer.Add(MyMarkerAdorner);
+                    MyMarkerAdorner.SetBinding(MarkerAdorner.MyXRateProperty, new Binding() { Source = this, Path = new PropertyPath(MySatProperty), Mode = BindingMode.TwoWay });
+                    MyMarkerAdorner.SetBinding(MarkerAdorner.MyYRateProperty, new Binding() { Source = this, Path = new PropertyPath(MyValProperty), Mode = BindingMode.TwoWay });
+                    MyMarkerAdorner.SetBinding(MarkerAdorner.MyMarkerSizeProperty, new Binding() { Source = this, Path = new PropertyPath(MyMarkerSizeProperty), Mode = BindingMode.TwoWay });
+
+                }
+            }
+
+            if (GetTemplateChild("PART_HSVImage") is HSVImage svi)
+            {
+                svi.SetBinding(HSVImage.HueProperty, new Binding() { Source = this, Path = new PropertyPath(MyHueProperty) });
+            }
+        }
+
+
+        #region 依存関係プロパティ
+
+
+        public double MyMarkerSize
+        {
+            get { return (double)GetValue(MyMarkerSizeProperty); }
+            set { SetValue(MyMarkerSizeProperty, value); }
+        }
+        public static readonly DependencyProperty MyMarkerSizeProperty =
+            DependencyProperty.Register(nameof(MyMarkerSize), typeof(double), typeof(Pikka), new PropertyMetadata(20.0));
+
+
+        public MarkerAdorner MyMarkerAdorner
+        {
+            get { return (MarkerAdorner)GetValue(MyMarkerAdornerProperty); }
+            private set { SetValue(MyMarkerAdornerProperty, value); }
+        }
+        public static readonly DependencyProperty MyMarkerAdornerProperty =
+            DependencyProperty.Register(nameof(MyMarkerAdorner), typeof(MarkerAdorner), typeof(Pikka), new PropertyMetadata(null));
+
+
+        public double MyHue
+        {
+            get { return (double)GetValue(MyHueProperty); }
+            set { SetValue(MyHueProperty, value); }
+        }
+        public static readonly DependencyProperty MyHueProperty =
+            DependencyProperty.Register(nameof(MyHue), typeof(double), typeof(Pikka), new PropertyMetadata(0.0));
+
+
+        public double MySat
+        {
+            get { return (double)GetValue(MySatProperty); }
+            set { SetValue(MySatProperty, value); }
+        }
+        public static readonly DependencyProperty MySatProperty =
+            DependencyProperty.Register(nameof(MySat), typeof(double), typeof(Pikka), new PropertyMetadata(0.0));
+
+
+        public double MyVal
+        {
+            get { return (double)GetValue(MyValProperty); }
+            set { SetValue(MyValProperty, value); }
+        }
+        public static readonly DependencyProperty MyValProperty =
+            DependencyProperty.Register(nameof(MyVal), typeof(double), typeof(Pikka), new PropertyMetadata(0.0));
+
+        #endregion 依存関係プロパティ
+
+    }
+
+
+
+
+
+    //public class HSVImageWithMarker : Control
+    //{
+    //    public HSVImage MyHSVImage { get; set; } = null!;
+    //    static HSVImageWithMarker()
+    //    {
+    //        DefaultStyleKeyProperty.OverrideMetadata(typeof(HSVImageWithMarker), new FrameworkPropertyMetadata(typeof(HSVImageWithMarker)));
+    //    }
+    //    public HSVImageWithMarker()
+    //    {
+    //        MySikisai = new SikisaiBrush();
+    //        Loaded += HSVImageWithMarker_Loaded;
+    //    }
+
+    //    private void HSVImageWithMarker_Loaded(object sender, RoutedEventArgs e)
+    //    {
+    //        // Adorner  Layer   結果
+    //        // Viewbox  Viewbox おｋ
+    //        // Viewbox  Image   おｋ
+    //        // Image    Viewbox クソデカMarker
+    //        // Image    Image   クソデカMarker
+
+    //        if (GetTemplateChild("PART_Viewbox") is Viewbox viewbox)
+    //        {
+    //            MyMarkerAdorner = new MarkerAdorner(viewbox);
+
+    //            // AdornerLayerを取得して、MarkerAdornerを設置
+    //            if (AdornerLayer.GetAdornerLayer(viewbox) is AdornerLayer layer)
+    //            {
+    //                layer.Add(MyMarkerAdorner);
+    //            }
+    //        }
+    //        if (GetTemplateChild("PART_HSVImage") is HSVImage HSVImage)
+    //        {
+    //            MyHSVImage = HSVImage;
+    //        }
+
+    //        MyBind();
+    //    }
+
+    //    private void MyBind()
+    //    {
+    //        MyMarkerAdorner.SetBinding(MarkerAdorner.MyXRateProperty, new Binding() { Source = MySikisai, Path = new PropertyPath(Sikisai.SProperty), Mode = BindingMode.TwoWay });
+    //        MyMarkerAdorner.SetBinding(MarkerAdorner.MyYRateProperty, new Binding() { Source = MySikisai, Path = new PropertyPath(Sikisai.VProperty), Mode = BindingMode.TwoWay });
+
+    //        //BindingOperations.SetBinding(MySikisai, Sikisai.SProperty, new Binding() { Source = MyMarkerAdorner, Path = new PropertyPath(MarkerAdorner.MySaturationProperty) });
+    //        //BindingOperations.SetBinding(MySikisai, Sikisai.VProperty, new Binding() { Source = MyMarkerAdorner, Path = new PropertyPath(MarkerAdorner.MyValueProperty) });
+    //        //BindingOperations.SetBinding(MySikisai, Sikisai.HProperty, new Binding() { Source = MyHSVImage, Path = new PropertyPath(HSVImage.HueProperty) });
+
+
+    //    }
+
+
+
+
+
+    //    #region 依存関係プロパティ
+
+    //    public SikisaiBrush MySikisai
+    //    {
+    //        get { return (SikisaiBrush)GetValue(MySikisaiProperty); }
+    //        set { SetValue(MySikisaiProperty, value); }
+    //    }
+    //    public static readonly DependencyProperty MySikisaiProperty =
+    //        DependencyProperty.Register(nameof(MySikisai), typeof(SikisaiBrush), typeof(HSVImageWithMarker), new PropertyMetadata(null));
+
+
+    //    public MarkerAdorner MyMarkerAdorner
+    //    {
+    //        get { return (MarkerAdorner)GetValue(MyMarkerAdornerProperty); }
+    //        set { SetValue(MyMarkerAdornerProperty, value); }
+    //    }
+    //    public static readonly DependencyProperty MyMarkerAdornerProperty =
+    //        DependencyProperty.Register(nameof(MyMarkerAdorner), typeof(MarkerAdorner), typeof(HSVImageWithMarker), new PropertyMetadata(null));
+    //    #endregion 依存関係プロパティ
+
+    //}
+
+
+
+
+
+
+
+    public class EllipseThumb : Thumb
+    {
+        static EllipseThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EllipseThumb), new FrameworkPropertyMetadata(typeof(EllipseThumb)));
+        }
+        public EllipseThumb()
+        {
+
+        }
+
+        #region 依存関係プロパティ
+
+        public Brush MyInsideStroke
+        {
+            get { return (Brush)GetValue(MyInsideStrokeProperty); }
+            set { SetValue(MyInsideStrokeProperty, value); }
+        }
+        public static readonly DependencyProperty MyInsideStrokeProperty =
+            DependencyProperty.Register(nameof(MyInsideStroke), typeof(Brush), typeof(EllipseThumb), new PropertyMetadata(Brushes.Black));
+
+        public Brush MyOutsideStroke
+        {
+            get { return (Brush)GetValue(MyOutsideStrokeProperty); }
+            set { SetValue(MyOutsideStrokeProperty, value); }
+        }
+        public static readonly DependencyProperty MyOutsideStrokeProperty =
+            DependencyProperty.Register(nameof(MyOutsideStroke), typeof(Brush), typeof(EllipseThumb), new PropertyMetadata(Brushes.White));
+
+
+        #endregion 依存関係プロパティ
+    }
+
+
+    public class MyConvInsideEllipseSize : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double size = ((double)value) - 2.0;
+            if (size < 0) { size = 0; }
+            return size;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 20250704_ColorPicker/HSVImage.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace _20250704_ColorPicker
+{
+
+
+
+
+
+    public class HSVImage : Image
+    {
+        public HSVImage()
+        {
+            Loaded += SVImage_Loaded;
+
+        }
+
+        private void SVImage_Loaded(object sender, RoutedEventArgs e)
+        {
+            MultiBinding mb;
+            mb = new() { Converter = new MyConvSVBitmap() };
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(WidthProperty) });
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(HeightProperty) });
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(HueProperty) });
+            _ = SetBinding(SourceProperty, mb);
+
+            //mb = new() { Converter = new MyConvHSVBrush() };
+            //mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(HueProperty) });
+            //mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(SProperty) });
+            //mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(VProperty) });
+            //_ = SetBinding(MyBrushProperty, mb);
+        }
+
+
+
+        public Brush MyBrush
+        {
+            get { return (Brush)GetValue(MyBrushProperty); }
+            set { SetValue(MyBrushProperty, value); }
+        }
+        public static readonly DependencyProperty MyBrushProperty =
+            DependencyProperty.Register(nameof(MyBrush), typeof(Brush), typeof(HSVImage), new PropertyMetadata(Brushes.Transparent));
+
+        public double MyHue
+        {
+            get { return (double)GetValue(HueProperty); }
+            set { SetValue(HueProperty, value); }
+        }
+        public static readonly DependencyProperty HueProperty =
+            DependencyProperty.Register(nameof(MyHue), typeof(double), typeof(HSVImage), new PropertyMetadata(0.0));
+
+
+
+
+        private class MyConvSVBitmap : IMultiValueConverter
+        {
+            private void MyParallel(int p, int y, int stride, byte[] pixels, double hue, int w, int h)
+            {
+                double v = y / (h - 1.0);
+                double ww = w - 1;
+
+                for (int x = 0; x < w; ++x)
+                {
+                    p = (y * stride) + (x * 3);
+                    (pixels[p + 2], pixels[p + 1], pixels[p]) = MathHSV.Hsv2rgb(hue, x / ww, v);
+                }
+            }
+            public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+            {
+                var width = (int)(double)values[0];
+                var height = (int)(double)values[1];
+                var hue = (double)values[2];
+
+                int stride = width * 3;
+                byte[] pixles = new byte[height * stride];
+
+                _ = Parallel.For(0, height, y =>
+                {
+                    MyParallel(0, y, stride, pixles, hue, width, height);
+                });
+
+                var bmp = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgr24, null);
+                bmp.WritePixels(new Int32Rect(0, 0, width, height), pixles, stride, 0);
+                return bmp;
+            }
+
+            public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class MyConvHSVBrush : IMultiValueConverter
+        {
+            public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+            {
+                var h = (double)values[0];
+                var s = (double)values[1];
+                var v = (double)values[2];
+                return new SolidColorBrush(MathHSV.hsv2Color(h, s, v));
+            }
+
+            public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
