34581af5dc1806b32e4652c72133ea16d37ddddc
waten <gogowaten@yahoo.co.jp>
2025-07-05 23:47:55 +0900

    202507052347

--- 20250704_ColorPicker/CustomControl1.cs
+++ 20250704_ColorPicker/CustomControl1.cs
@@ -22,11 +22,40 @@ namespace _20250704_ColorPicker
 
 
 
+    // PikkaにRGBをバインドしたブラシを追加したもの
+    public class PikkaBrush : Pikka
+    {
+        public PikkaBrush() { MyBind(); }
+        private void MyBind()
+        {
+            MultiBinding mb;
+            mb = new() { Converter = new MyConvRGBSolidColorBrush() };
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(MyRProperty) });
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(MyGProperty) });
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(MyBProperty) });
+            _ = SetBinding(MySolidColorBrushProperty, mb);
+        }
+
+
+        public SolidColorBrush MySolidColorBrush
+        {
+            get { return (SolidColorBrush)GetValue(MySolidColorBrushProperty); }
+            set { SetValue(MySolidColorBrushProperty, value); }
+        }
+        public static readonly DependencyProperty MySolidColorBrushProperty =
+            DependencyProperty.Register(nameof(MySolidColorBrush), typeof(SolidColorBrush), typeof(Pikka), new PropertyMetadata(null));
+    }
 
 
 
+
+    // カラーピッカー
     public class Pikka : Control
     {
+        public enum MyCurrentChangingColorType { None = 0, Rgb, Hsv, }
+
+        public MyCurrentChangingColorType CurrentChangingColorType { get; private set; }
+
         static Pikka()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(Pikka), new FrameworkPropertyMetadata(typeof(Pikka)));
@@ -34,7 +63,7 @@ namespace _20250704_ColorPicker
 
         public Pikka()
         {
-
+            
         }
 
 
@@ -47,23 +76,32 @@ namespace _20250704_ColorPicker
                 {
                     MyMarkerAdorner = new MarkerAdorner(vb);
                     layer.Add(MyMarkerAdorner);
-                    MyMarkerAdorner.SetBinding(MarkerAdorner.MyXRateProperty, new Binding() { Source = this, Path = new PropertyPath(MySatProperty), Mode = BindingMode.TwoWay });
-                    MyMarkerAdorner.SetBinding(MarkerAdorner.MyYRateProperty, new Binding() { Source = this, Path = new PropertyPath(MyValProperty), Mode = BindingMode.TwoWay });
-                    MyMarkerAdorner.SetBinding(MarkerAdorner.MyMarkerSizeProperty, new Binding() { Source = this, Path = new PropertyPath(MyMarkerSizeProperty), Mode = BindingMode.TwoWay });
-
+                    _ = MyMarkerAdorner.SetBinding(MarkerAdorner.MyXRateProperty, new Binding() { Source = this, Path = new PropertyPath(MySatProperty), Mode = BindingMode.TwoWay });
+                    _ = MyMarkerAdorner.SetBinding(MarkerAdorner.MyYRateProperty, new Binding() { Source = this, Path = new PropertyPath(MyValProperty), Mode = BindingMode.TwoWay });
+                    _ = MyMarkerAdorner.SetBinding(MarkerAdorner.MyMarkerSizeProperty, new Binding() { Source = this, Path = new PropertyPath(MyMarkerSizeProperty), Mode = BindingMode.TwoWay });
+                    
                 }
             }
 
             if (GetTemplateChild("PART_HSVImage") is HSVImage svi)
             {
-                svi.SetBinding(HSVImage.HueProperty, new Binding() { Source = this, Path = new PropertyPath(MyHueProperty) });
+                _ = svi.SetBinding(HSVImage.HueProperty, new Binding() { Source = this, Path = new PropertyPath(MyHueProperty) });
             }
         }
 
 
         #region 依存関係プロパティ
 
+        // 画像サイズ、拡大表示されるので16でも十分。全色表示するには256を指定して、Pikkaのサイズも256にする
+        public double MyHSVImageSize
+        {
+            get { return (double)GetValue(MyHSVImageSizeProperty); }
+            set { SetValue(MyHSVImageSizeProperty, value); }
+        }
+        public static readonly DependencyProperty MyHSVImageSizeProperty =
+            DependencyProperty.Register(nameof(MyHSVImageSize), typeof(double), typeof(Pikka), new PropertyMetadata(16.0));
 
+        // Markerのサイズ
         public double MyMarkerSize
         {
             get { return (double)GetValue(MyMarkerSizeProperty); }
@@ -82,13 +120,16 @@ namespace _20250704_ColorPicker
             DependencyProperty.Register(nameof(MyMarkerAdorner), typeof(MarkerAdorner), typeof(Pikka), new PropertyMetadata(null));
 
 
+
+        #region HSV
+
         public double MyHue
         {
             get { return (double)GetValue(MyHueProperty); }
             set { SetValue(MyHueProperty, value); }
         }
         public static readonly DependencyProperty MyHueProperty =
-            DependencyProperty.Register(nameof(MyHue), typeof(double), typeof(Pikka), new PropertyMetadata(0.0));
+            DependencyProperty.Register(nameof(MyHue), typeof(double), typeof(Pikka), new FrameworkPropertyMetadata(0.0, new PropertyChangedCallback(OnChangedHSV)));
 
 
         public double MySat
@@ -97,7 +138,7 @@ namespace _20250704_ColorPicker
             set { SetValue(MySatProperty, value); }
         }
         public static readonly DependencyProperty MySatProperty =
-            DependencyProperty.Register(nameof(MySat), typeof(double), typeof(Pikka), new PropertyMetadata(0.0));
+            DependencyProperty.Register(nameof(MySat), typeof(double), typeof(Pikka), new FrameworkPropertyMetadata(0.0, new PropertyChangedCallback(OnChangedHSV)));
 
 
         public double MyVal
@@ -106,9 +147,116 @@ namespace _20250704_ColorPicker
             set { SetValue(MyValProperty, value); }
         }
         public static readonly DependencyProperty MyValProperty =
-            DependencyProperty.Register(nameof(MyVal), typeof(double), typeof(Pikka), new PropertyMetadata(0.0));
+            DependencyProperty.Register(nameof(MyVal), typeof(double), typeof(Pikka), new FrameworkPropertyMetadata(0.0, new PropertyChangedCallback(OnChangedHSV)));
+        #endregion HSV
+
+        #region RGB
+
+        public byte MyR
+        {
+            get { return (byte)GetValue(MyRProperty); }
+            set { SetValue(MyRProperty, value); }
+        }
+        public static readonly DependencyProperty MyRProperty =
+            DependencyProperty.Register(nameof(MyR), typeof(byte), typeof(Pikka), new FrameworkPropertyMetadata((byte)0, new PropertyChangedCallback(OnChangedRGB)));
+
+        public byte MyG
+        {
+            get { return (byte)GetValue(MyGProperty); }
+            set { SetValue(MyGProperty, value); }
+        }
+        public static readonly DependencyProperty MyGProperty =
+            DependencyProperty.Register(nameof(MyG), typeof(byte), typeof(Pikka), new FrameworkPropertyMetadata((byte)0, new PropertyChangedCallback(OnChangedRGB)));
+
+        public byte MyB
+        {
+            get { return (byte)GetValue(MyBProperty); }
+            set { SetValue(MyBProperty, value); }
+        }
+        public static readonly DependencyProperty MyBProperty =
+            DependencyProperty.Register(nameof(MyB), typeof(byte), typeof(Pikka), new FrameworkPropertyMetadata((byte)0, new PropertyChangedCallback(OnChangedRGB)));
+        #endregion RGB
+
+        //#region HSV
+
+        //public double MyHue
+        //{
+        //    get { return (double)GetValue(MyHueProperty); }
+        //    set { SetValue(MyHueProperty, value); }
+        //}
+        //public static readonly DependencyProperty MyHueProperty =
+        //    DependencyProperty.Register(nameof(MyHue), typeof(double), typeof(Pikka), new PropertyMetadata(0.0));
+
+
+        //public double MySat
+        //{
+        //    get { return (double)GetValue(MySatProperty); }
+        //    set { SetValue(MySatProperty, value); }
+        //}
+        //public static readonly DependencyProperty MySatProperty =
+        //    DependencyProperty.Register(nameof(MySat), typeof(double), typeof(Pikka), new PropertyMetadata(0.0));
+
+
+        //public double MyVal
+        //{
+        //    get { return (double)GetValue(MyValProperty); }
+        //    set { SetValue(MyValProperty, value); }
+        //}
+        //public static readonly DependencyProperty MyValProperty =
+        //    DependencyProperty.Register(nameof(MyVal), typeof(double), typeof(Pikka), new PropertyMetadata(0.0));
+        //#endregion HSV
+
+        //#region RGB
+
+        //public byte MyR
+        //{
+        //    get { return (byte)GetValue(MyRProperty); }
+        //    set { SetValue(MyRProperty, value); }
+        //}
+        //public static readonly DependencyProperty MyRProperty =
+        //    DependencyProperty.Register(nameof(MyR), typeof(byte), typeof(Pikka), new PropertyMetadata((byte)0));
+
+        //public byte MyG
+        //{
+        //    get { return (byte)GetValue(MyGProperty); }
+        //    set { SetValue(MyGProperty, value); }
+        //}
+        //public static readonly DependencyProperty MyGProperty =
+        //    DependencyProperty.Register(nameof(MyG), typeof(byte), typeof(Pikka), new PropertyMetadata((byte)0));
+
+        //public byte MyB
+        //{
+        //    get { return (byte)GetValue(MyBProperty); }
+        //    set { SetValue(MyBProperty, value); }
+        //}
+        //public static readonly DependencyProperty MyBProperty =
+        //    DependencyProperty.Register(nameof(MyB), typeof(byte), typeof(Pikka), new PropertyMetadata((byte)0));
+        //#endregion RGB
 
         #endregion 依存関係プロパティ
+
+        #region 依存関係プロパティの変更後に実行する関数
+        
+        private static void OnChangedRGB(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Pikka pk && pk.CurrentChangingColorType == MyCurrentChangingColorType.None)
+            {
+                pk.CurrentChangingColorType = MyCurrentChangingColorType.Rgb;
+                (pk.MyHue, pk.MySat, pk.MyVal) = MathHSV.Rgb2hsv(pk.MyR, pk.MyG, pk.MyB);
+                pk.CurrentChangingColorType = MyCurrentChangingColorType.None;
+            }
+        }
+
+        private static void OnChangedHSV(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Pikka pk && pk.CurrentChangingColorType == MyCurrentChangingColorType.None)
+            {
+                pk.CurrentChangingColorType = MyCurrentChangingColorType.Hsv;
+                (pk.MyR, pk.MyG, pk.MyB) = MathHSV.Hsv2rgb(pk.MyHue, pk.MySat, pk.MyVal);
+                pk.CurrentChangingColorType = MyCurrentChangingColorType.None;
+            }
+        }
+        #endregion 依存関係プロパティの変更後に実行する関数
 
     }
 
@@ -233,6 +381,25 @@ namespace _20250704_ColorPicker
     }
 
 
+    // RGBからブラシ作成
+    public class MyConvRGBSolidColorBrush : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var r = (byte)values[0];
+            var g = (byte)values[1];
+            var b = (byte)values[2];
+            return new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    // -2するだけ
     public class MyConvInsideEllipseSize : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
